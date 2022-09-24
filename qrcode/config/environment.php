1243b32de148b7d5dc29056bfa3922ef255e65ac
Giandonato Inverso <70962472+giandonatoinverso@users.noreply.github.com>
2022-09-24 03:23:20 +0200

    Update environment.php

--- qrcode/config/environment.php
+++ qrcode/config/environment.php
@@ -12,7 +12,6 @@ if(is_string($docker_cid)) {
     define('DATABASE_NAME', Getenv('DATABASE_NAME'));
     define('DATABASE_USER', Getenv('DATABASE_USER'));
     define('DATABASE_PASSWORD', Getenv('DATABASE_PASSWORD'));
-    define('DATABASE_PREFIX', Getenv('DATABASE_PREFIX'));
     define('DATABASE_CHARSET', Getenv('DATABASE_CHARSET'));
 } else {
     define('DATABASE_HOST', "localhost");
@@ -20,7 +19,6 @@ if(is_string($docker_cid)) {
     define('DATABASE_NAME', "qrcode");
     define('DATABASE_USER', "root");
     define('DATABASE_PASSWORD', "root");
-    define('DATABASE_PREFIX', "");
     define('DATABASE_CHARSET', "utf8");
 }
 /*
