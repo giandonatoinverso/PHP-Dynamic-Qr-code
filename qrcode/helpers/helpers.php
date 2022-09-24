f064d8f1efffde0b205a025706d3b2a862a7246a
0xRenegade <therenegadecoding@gmail.com>
2022-09-24 14:32:04 -0500

    add base_url() function in helpers, works with https and port number

--- qrcode/helpers/helpers.php
+++ qrcode/helpers/helpers.php
@@ -116,3 +116,12 @@ function paginationLinks($current_page, $total_pages, $base_url) {
 
 	return $html;
 }
+
+function base_url(){
+    return sprintf(
+        "%s://%s:%s",
+        isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+        $_SERVER['SERVER_NAME'],
+        $_SERVER['SERVER_PORT']
+    );
+}
