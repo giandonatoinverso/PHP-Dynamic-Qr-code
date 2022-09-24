cccf3ada437bf9f41103198ec43ab88595ec5504
0xRenegade <therenegadecoding@gmail.com>
2022-09-24 13:09:17 -0500

    adds in custom.css for, well, custom styles.

--- qrcode/includes/head.php
+++ qrcode/includes/head.php
@@ -16,3 +16,5 @@
   <link type="text/css" href="plugins/overlayScrollbars/css/OverlayScrollbars.css" rel="stylesheet"/>
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="dist/css/custom.css">
