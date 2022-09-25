0d9036740e21d9c6f0fa88e005cb525ed0ca12a8
0xRenegade <therenegadecoding@gmail.com>
2022-09-25 14:30:28 -0500

    set qrcode type separate from action

--- qrcode/bulk_action.php
+++ qrcode/bulk_action.php
@@ -16,11 +16,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $db = getDbInstance();
     $json = json_decode(file_get_contents('php://input'), true);
     $action = filter_var($json['action'], FILTER_SANITIZE_STRING);
+    $type = filter_var($json['type'], FILTER_SANITIZE_STRING);
 
     if ($action === 'download') {
         $params = $json['params'];
         $files = [];
-        $type = filter_var($json['type'], FILTER_SANITIZE_STRING);
 
         if (count($params) == 0) {
             echo json_encode([
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'status' => 200
         ]);
         exit();
-    } 
+    }
 } else {
     exit('Direct access to this script not allowed.');
 }
