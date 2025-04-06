cee6f4d3d50849db0de2ce0e7751634c9787ca34
Angelo Sleebos <angelosleebos@yahoo.nl>
2025-04-07 01:11:17 +0200

    Make environment variables compatible for other platforms
    Make environment variables compatible for other platforms

--- src/config/environment.php
+++ src/config/environment.php
@@ -1,35 +1,18 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| DOCKER INSTALLATION
+| UNIFIED ENVIRONMENT CONFIGURATION
 |--------------------------------------------------------------------------
- */
+*/
 
-if(is_string(Getenv('TYPE')) && Getenv('TYPE') == "docker") {
-    define('DATABASE_HOST', Getenv('DATABASE_HOST'));
-    define('DATABASE_PORT', filter_var(Getenv('DATABASE_PORT'), FILTER_VALIDATE_INT));
-    define('DATABASE_NAME', Getenv('DATABASE_NAME'));
-    define('DATABASE_USER', Getenv('DATABASE_USER'));
-    define('DATABASE_PASSWORD', Getenv('DATABASE_PASSWORD'));
-    define('DATABASE_PREFIX', Getenv('DATABASE_PREFIX'));
-    define('DATABASE_CHARSET', Getenv('DATABASE_CHARSET'));
-    define('TYPE', Getenv('TYPE'));
-    define('BASE_URL', Getenv('BASE_URL'));
-    define("QRCODE_GENERATOR", Getenv('QRCODE_GENERATOR'));
-} else {
-    define('DATABASE_HOST', "localhost");
-    define('DATABASE_PORT', "3306");
-    define('DATABASE_NAME', "qrcode");
-    define('DATABASE_USER', "root");
-    define('DATABASE_PASSWORD', "root");
-    define('DATABASE_PREFIX', "qr_");
-    define('DATABASE_CHARSET', "utf8");
-    define("QRCODE_GENERATOR", "external-api.qrserver.com");  // external-api.qrserver.com  => https://api.qrserver.com/v1/create-qr-code/?data=    //  internal-chillerlan.qrcode => https://github.com/chillerlan/php-qrcode
-}
-/*
-|--------------------------------------------------------------------------
-| INSTALLATION WITHOUT CONTAINER
-|--------------------------------------------------------------------------
- */
+define('DATABASE_HOST', getenv('DATABASE_HOST') ?: 'localhost');
+define('DATABASE_PORT', filter_var(getenv('DATABASE_PORT'), FILTER_VALIDATE_INT) ?: 3306);
+define('DATABASE_NAME', getenv('DATABASE_NAME') ?: 'qrcode');
+define('DATABASE_USER', getenv('DATABASE_USER') ?: 'root');
+define('DATABASE_PASSWORD', getenv('DATABASE_PASSWORD') ?: 'root');
+define('DATABASE_PREFIX', getenv('DATABASE_PREFIX') !== false ? getenv('DATABASE_PREFIX') : 'qr_');
+define('DATABASE_CHARSET', getenv('DATABASE_CHARSET') ?: 'utf8');
 
-?>
+define('TYPE', getenv('TYPE') ?: 'local');
+define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost');
+define('QRCODE_GENERATOR', getenv('QRCODE_GENERATOR') ?: 'external-api.qrserver.com'); // opties: external-api.qrserver.com of internal-chillerlan.qrcode
